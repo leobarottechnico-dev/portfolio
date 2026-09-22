36a248c4aebfb1dc2a02a4f29f7aa73d1b9d7e12
leobarotramonas-del <leobarotramonas-del@users.noreply.github.com>
2026-09-22 14:40:37 +0800

    Record Sanity Studio deployment

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -1,2 +1,6 @@
 import { defineCliConfig } from 'sanity/cli';
-export default defineCliConfig({api:{projectId:'cygey76w',dataset:'production'}});
+
+export default defineCliConfig({
+  api: { projectId: 'cygey76w', dataset: 'production' },
+  deployment: { appId: 'xtbqf45f4uugr2mwvpk13y4o' },
+});
